7e013bc0f7ae2d9ab18beea3a2a575f04fde4011
caine tighe <caine@duckduckgo.com>
2012-01-30 18:24:24 -0500

    init project files...

---
+++ src/com/duckduckgo/mobile/android/ArticlesActivity.java
@@ -0,0 +1,15 @@
+package com.duckduckgo.mobile.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ArticlesActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
