286025ce1c16463375ff9fb9447eead3d801f6c6
caine tighe <caine@duckduckgo.com>
2012-02-05 09:51:27 -0500

    Fix SEARCH_URL constant.

--- src/com/duckduckgo/mobile/android/DDGConstants.java
+++ src/com/duckduckgo/mobile/android/DDGConstants.java
@@ -3,5 +3,5 @@ package com.duckduckgo.mobile.android;
 public class DDGConstants {
 	static public final String USER_AGENT = "DDG-Android-2.0.0a";
 	static public final String AUTO_COMPLETE_URL = "http://swass.duckduckgo.com:6767/face/suggest/?q=";
-	static public final String SEARCH_URL = "http://www.duckduckgo.com/?q=?";
+	static public final String SEARCH_URL = "http://www.duckduckgo.com/?q=";
 }
