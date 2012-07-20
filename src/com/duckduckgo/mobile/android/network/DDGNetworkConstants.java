4889e0fd43030f1527aebc514c22f8dacdd6436c
Çağatay Çallı <ccalli@gmail.com>
2012-07-20 15:58:23 +0300

    HttpClient : custom schemeRegistry used while instantiating ThreadSafeClientConnManager is not needed. Using default ThreadSafeClientConnManager instead.

--- src/com/duckduckgo/mobile/android/network/DDGNetworkConstants.java
+++ src/com/duckduckgo/mobile/android/network/DDGNetworkConstants.java
@@ -3,9 +3,6 @@ package com.duckduckgo.mobile.android.network;
 import ch.boye.httpclientandroidlib.conn.ClientConnectionManager;
 import ch.boye.httpclientandroidlib.conn.params.ConnManagerParams;
 import ch.boye.httpclientandroidlib.conn.params.ConnPerRouteBean;
-import ch.boye.httpclientandroidlib.conn.scheme.PlainSocketFactory;
-import ch.boye.httpclientandroidlib.conn.scheme.Scheme;
-import ch.boye.httpclientandroidlib.conn.scheme.SchemeRegistry;
 import ch.boye.httpclientandroidlib.impl.conn.tsccm.ThreadSafeClientConnManager;
 import ch.boye.httpclientandroidlib.params.BasicHttpParams;
 import ch.boye.httpclientandroidlib.params.HttpParams;
@@ -22,15 +19,10 @@ public class DDGNetworkConstants {
         // Increase default max connection per route to 20
         ConnManagerParams.setMaxConnectionsPerRoute(httpParams, new ConnPerRouteBean(10));
          
-        // Create and initialize scheme registry
-        SchemeRegistry schemeRegistry = new SchemeRegistry();
-        schemeRegistry.register(
-                new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-         
         // Create an HttpClient with the ThreadSafeClientConnManager.
         // This connection manager must be used if more than one thread will
         // be using the HttpClient.
-        mainConnManager = new ThreadSafeClientConnManager(httpParams, schemeRegistry);
+        mainConnManager = new ThreadSafeClientConnManager();
         mainClient = new DDGHttpClient(mainConnManager, httpParams);
 	}
 	
