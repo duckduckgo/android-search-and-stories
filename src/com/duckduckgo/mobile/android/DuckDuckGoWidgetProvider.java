a45f2d701b66393e6f30880c35e41a880650ecd1
Çağatay Çallı <ccalli@gmail.com>
2012-07-24 00:28:35 +0300

    Widget : ported/added from old DuckDuckGo app

---
+++ src/com/duckduckgo/mobile/android/DuckDuckGoWidgetProvider.java
@@ -0,0 +1,32 @@
+package com.duckduckgo.mobile.android;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class DuckDuckGoWidgetProvider extends AppWidgetProvider {
+
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, DuckDuckGo.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+            views.setOnClickPendingIntent(R.id.widget_go_btn, pendingIntent);
+            //views.setOnClickPendingIntent(R.id.widget_voice_btn, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current App Widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
