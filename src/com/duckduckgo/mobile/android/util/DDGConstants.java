424828dd6d9a4ec14bfeb7a939559a7504a000e2
Çağatay Çallı <ccalli@gmail.com>
2012-12-17 23:23:05 +0200

    HTTP->HTTPS for search and autocomplete added Readability URL to constants

--- src/com/duckduckgo/mobile/android/util/DDGConstants.java
+++ src/com/duckduckgo/mobile/android/util/DDGConstants.java
@@ -6,10 +6,11 @@ public class DDGConstants {
 	public static String USER_AGENT = "DDG-Android-%version";
 //	public static final String USER_AGENT = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)";
 	public static final String AUTO_COMPLETE_URL = "https://www.duckduckgo.com/ac/?q=";
-	public static final String SEARCH_URL = "http://www.duckduckgo.com/?ko=-1&q=";
+	public static final String SEARCH_URL = "https://www.duckduckgo.com/?ko=-1&q=";
 	public static final String MAIN_FEED_URL = "http://watrcoolr.duckduckgo.com/watrcoolr.js?o=json";
 	public static final String SOURCES_URL = "http://watrcoolr.duckduckgo.com/watrcoolr.js?o=json&type_info=1";
 	public static final String ICON_LOOKUP_URL = "http://i2.duck.co/i/";
+	public static final String READABILITY_URL = "http://www.readability.com/read?url=";
 	
 	public static final String STORIES_JSON_PATH = "stories.json";
 	public static final String SOURCE_JSON_PATH = "source.json";
