ea767d9c163480e9b1e3cf66818b957ba6efd246
koenmetsu <koen.metsu@gmail.com>
2013-03-19 23:13:24 +0100

    #2717382704756: changed format as requested for sharing Stories

--- src/com/duckduckgo/mobile/android/util/DDGUtils.java
+++ src/com/duckduckgo/mobile/android/util/DDGUtils.java
@@ -437,11 +437,15 @@ public final class DDGUtils {
 	  public static void shareStory(Context context, String title, String url) {
 		  Intent sendIntent = new Intent();
 			sendIntent.setAction(Intent.ACTION_SEND);
-			sendIntent.putExtra(Intent.EXTRA_TEXT, "Story link via DuckDuckGo for Android: "+ url);
+			sendIntent.putExtra(Intent.EXTRA_TEXT, formatShareText(context, title, url));
 			sendIntent.putExtra(Intent.EXTRA_SUBJECT, title);
 			sendIntent.setType("text/plain");
 			context.startActivity(Intent.createChooser(sendIntent, context.getResources().getText(R.string.ShareStory)));
 	  }
+
+	private static String formatShareText(Context context, String title, String url) {
+		return String.format(context.getResources().getString(R.string.ShareFormat), title, url);
+	}
 	  
 	  public static void shareSavedSearch(Context context, String query) {
 		  shareSavedSearch(context, query, "https://duckduckgo.com/?q=" + query);
