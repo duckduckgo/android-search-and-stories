87aa97aa127d59ed18c9d9cd314555de860cde94
Çağatay Çallı <ccalli@gmail.com>
2013-04-16 01:33:45 +0300

    disabled Welcome screen for now, will re-enable by incrementing WELCOME_VERSION when ready

--- src/com/duckduckgo/mobile/android/util/PreferencesManager.java
+++ src/com/duckduckgo/mobile/android/util/PreferencesManager.java
@@ -7,7 +7,7 @@ import android.content.SharedPreferences.Editor;
 
 public class PreferencesManager {
 	
-	private static int WELCOME_VERSION = 1;
+	private static int WELCOME_VERSION = 0;
 	
 	/* Settings */
 	
