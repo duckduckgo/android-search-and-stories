8bd41e12827ec2fbde43103d34e32b72b148d247
Kevin Pelgrims <kp@clickataxi.com>
2013-04-18 23:04:38 +0200

    Fix for item click in saved search results

--- src/com/duckduckgo/mobile/android/fragment/SavedResultTabFragment.java
+++ src/com/duckduckgo/mobile/android/fragment/SavedResultTabFragment.java
@@ -1,15 +1,18 @@
 package com.duckduckgo.mobile.android.fragment;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.duckduckgo.mobile.android.R;
 import com.duckduckgo.mobile.android.activity.DuckDuckGo;
+import com.duckduckgo.mobile.android.adapters.SavedResultCursorAdapter;
 import com.duckduckgo.mobile.android.views.SavedSearchListView;
 import com.duckduckgo.mobile.android.views.SavedSearchListView.OnSavedSearchItemSelectedListener;
 
@@ -50,6 +53,25 @@ public class SavedResultTabFragment extends ListFragment {
 			});
 			
 			savedSearchView.setOnSavedSearchItemLongClickListener(((DuckDuckGo) activity).mSavedSearchLongClickListener);
+		}
+	}
+
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		
+		final Activity activity = getActivity();
+		Object adapter = getListView().getAdapter();
+		Cursor c = null;
+		
+		if(adapter instanceof SavedResultCursorAdapter) {
+			c = (Cursor) ((SavedResultCursorAdapter) adapter).getItem(position);
+			String query = c.getString(c.getColumnIndex("query"));
+			if(query != null){							
+				((DuckDuckGo) activity).searchWebTerm(query);	
+				((DuckDuckGo) activity).itemSaveSearch(query);
+				((DuckDuckGo) activity).syncAdapters();
+			}
 		}
 	}
 }
