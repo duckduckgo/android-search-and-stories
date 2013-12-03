90226c7590283e9a472dd29d42cb4a72ea667123
Çağatay Çallı <ccalli@gmail.com>
2013-12-03 21:53:34 +0200

    cleaned up artifacts related to unused DDGPreferenceFragment

--- src/com/duckduckgo/mobile/android/activity/DuckDuckGo.java
+++ src/com/duckduckgo/mobile/android/activity/DuckDuckGo.java
@@ -898,9 +898,6 @@ public class DuckDuckGo extends FragmentActivity implements OnClickListener {
 				case SCR_SAVED_FEED:
 					displaySavedFeed();
 					break;
-				case SCR_SETTINGS:
-					displaySettings();
-					break;
 				default:
 					break;
 			}
@@ -1018,10 +1015,6 @@ public class DuckDuckGo extends FragmentActivity implements OnClickListener {
 		}
 		else if (mDuckDuckGoContainer.webviewShowing) {
 			mainWebView.backPressAction();
-		}
-		else if(mDuckDuckGoContainer.currentScreen == SCREEN.SCR_SETTINGS){
-			// go back to where we left of
-			displayScreen(mDuckDuckGoContainer.prevScreen, false);
 		}
 		else if(fontSizeLayout.getVisibility() != View.GONE) {
 			cancelFontScaling();
@@ -1227,13 +1220,11 @@ public class DuckDuckGo extends FragmentActivity implements OnClickListener {
 	/**
 	 * main method that triggers display of Preferences screen or fragment
 	 */
-    private void displaySettings() {
-        if(!((mDuckDuckGoContainer.currentScreen == SCREEN.SCR_SETTINGS))){
-            feedView.cleanImageTasks();
-            Intent intent = new Intent(getBaseContext(), Preferences.class);
-            startActivityForResult(intent, PREFERENCES_RESULT);
-        }
-    }
+	private void displaySettings() {
+		feedView.cleanImageTasks();
+		Intent intent = new Intent(getBaseContext(), Preferences.class);
+		startActivityForResult(intent, PREFERENCES_RESULT);
+	}
 
 	/** 
 	 * change button visibility in left-side navigation menu
@@ -1529,13 +1520,7 @@ public class DuckDuckGo extends FragmentActivity implements OnClickListener {
 			return;
 		}
 		
-		// arbitrary choice to not display Settings on comeback
-    	if(mDuckDuckGoContainer.currentScreen == SCREEN.SCR_SETTINGS) {
-    		displayHomeScreen();
-    	}
-    	else {
-			displayScreen(mDuckDuckGoContainer.currentScreen, true);
-    	}
+		displayScreen(mDuckDuckGoContainer.currentScreen, true);
 	}
 	
 	private void markLeftSelect(SCREEN current){
@@ -1557,9 +1542,6 @@ public class DuckDuckGo extends FragmentActivity implements OnClickListener {
 					break;
 				case SCR_SAVED_FEED:
 					leftSavedTextView.setSelected(true);
-					break;
-				case SCR_SETTINGS:
-					leftSettingsTextView.setSelected(true);
 					break;
 			}
 		}
