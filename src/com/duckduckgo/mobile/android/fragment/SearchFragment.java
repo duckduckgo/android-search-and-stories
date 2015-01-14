887603e3291a9a094a7f4c18baca05044c694d89
fgei <gei.fff@gmail.com>
2015-01-14 21:08:40 +0100

    search fragment show one saved search with keyboard open

--- src/com/duckduckgo/mobile/android/fragment/SearchFragment.java
+++ src/com/duckduckgo/mobile/android/fragment/SearchFragment.java
@@ -2,9 +2,13 @@ package com.duckduckgo.mobile.android.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.duckduckgo.mobile.android.DDGApplication;
 import com.duckduckgo.mobile.android.R;
@@ -26,6 +30,7 @@ public class SearchFragment extends Fragment {
     private SavedSearchListView savedSearchListView;
     private SavedResultCursorAdapter savedSearchAdapter;
 
+    private LinearLayout search_container;
     private View fragmentView = null;
 
     @Override
@@ -50,10 +55,14 @@ public class SearchFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        search_container = (LinearLayout) fragmentView.findViewById(R.id.search_container);
+
         recentSearchListView = (RecentSearchListView) fragmentView.findViewById(R.id.recentList);
         recentSearchListView.setDivider(null);
         recentSearchAdapter = new RecentResultCursorAdapter(getActivity(), DDGApplication.getDB().getCursorSearchHistory());
         recentSearchListView.setAdapter(recentSearchAdapter);
+
+        setMaxItemVisible(recentSearchListView);
 
         savedSearchListView = (SavedSearchListView) fragmentView.findViewById(R.id.savedList);
         savedSearchListView.setDivider(null);
@@ -67,6 +76,58 @@ public class SearchFragment extends Fragment {
         recentSearchAdapter.notifyDataSetChanged();
         savedSearchAdapter.changeCursor(DDGApplication.getDB().getCursorSavedSearch());
         savedSearchAdapter.notifyDataSetChanged();
+    }
+
+    public void setMaxItemVisible(final ListView listView) {
+        Log.e("aaa", "--------------------inside calculate visible item");
+        final View container = search_container;
+        container.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+
+
+                //Log.e("aaa", "total height: "+container.getRootView().getHeight());
+                //Log.e("aaa", "visible height: "+container.getHeight());
+                int totalHeight = container.getRootView().getHeight();
+                int visibleHeight = container.getHeight();
+                if(2*visibleHeight<totalHeight) {
+                    //Log.e("aaa", "open");
+
+                    View item = listView.getAdapter().getView(0, null, listView);
+                    item.measure(0, 0);
+                    int itemHeight = (item.getMeasuredHeight() + listView.getDividerHeight());
+                    //Log.e("aaa", "item height: " + itemHeight);
+
+                    int maxItems = (int) (visibleHeight / itemHeight);
+                    maxItems = maxItems - 1;
+                    //Log.e("aaa", "max items: " + maxItems);
+
+                    ViewGroup.LayoutParams params = listView.getLayoutParams();
+                    //params.height = itemHeight * maxItems;
+
+                    int oldHeight = params.height;
+                    int newHeight = (itemHeight * maxItems);
+
+                    //Log.e("aaa", "total list height: " + params.height);
+                    //Log.e("aaa", "new list height: " + (itemHeight * maxItems));
+
+                    params.height = (itemHeight * maxItems);
+                    listView.setLayoutParams(params);
+                    listView.requestLayout();
+
+                } else {
+                    //Log.e("aaa", "close");
+
+                    ViewGroup.LayoutParams params = listView.getLayoutParams();
+                    params.height = -2;
+                    listView.setLayoutParams(params);
+                    listView.requestLayout();
+
+                }
+
+            }
+        });
+
     }
 
 
